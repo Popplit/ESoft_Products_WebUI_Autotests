9bbbd9fc28a84a5cb867491945f5a3da5212eaed
Popplit <sashadesh9@gmail.com>
2024-05-28 12:08:03 +0700

    Added Logger and ReportingExtension

---
+++ src/main/java/logger/LoggerClass.java
@@ -0,0 +1,8 @@
+package logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerClass {
+    public static final Logger LOGGER = LoggerFactory.getLogger(LoggerClass.class);
+}
